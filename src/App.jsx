960f213c7b13d7fcc53cd8a2c23f369264b8b807
Carlos Andres <shark9021@gmail.com>
2024-06-06 22:55:38 -0500

    LocalStorage save and read phase 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,81 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import { AgmEditor } from './Components/AgmEditor'
 
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pageSelected, setpageSelected] = useState({name:'welcome' , id:-1});
+  const [pages, setPages] = useState([]);
+
+  const editableTitle = useRef(null);
+
+  const addNewPage = function () {
+
+    if (editableTitle.current) {
+      setTimeout(() => {
+        // Borrar el contenido del div
+        editableTitle.current.innerHTML = '';
+        // Enfocar el div
+        editableTitle.current.focus();
+
+        const range = document.createRange();
+        const selection = window.getSelection();
+        range.setStart(editableTitle, editableTitle.childNodes.length);
+        range.collapse(true);
+        selection.removeAllRanges();
+        selection.addRange(range);
+      }, 150);
+    }
+
+    setpageSelected({name:'blank' , id:0});
+  }
+
+  const startsWithEmoji = (word) => {
+    // Expresión regular para verificar si el primer carácter es un emoji
+    const emojiRegex = /^\p{Emoji}/u;
+    return emojiRegex.test(word);
+  }
+
+
+  const handleBlur = () => {
+    let text = editableTitle.current.innerText;
+
+
+
+    if (text.length < 3)
+      return;
+    
+    text = (startsWithEmoji(text)) ? text : `🅰️ ${text}`;
+
+    if (pages.some(x => x.name == text))
+      return;
+
+    const newPage = { 'id': uuidv4(), 'name': text }
+    let newPages = [...pages, newPage];
+    setPages(newPages);
+    console.log({ newPages });
+    editableTitle.current.innerHTML = text;
+
+    localStorage.setItem('pages', JSON.stringify(newPages));
+    setpageSelected(newPage.id);
+  }
+  
+  const selectPage = (page)=>{
+    setpageSelected(page);
+  }
+
+
+  useEffect(() => {
+    console.log({ pageSelected });
+    let actualPages = localStorage.getItem('pages');
+
+    if (actualPages) {
+      setPages(JSON.parse(actualPages));
+    }
+
+  }, [pageSelected]);
+
 
   return (
     <>
@@ -13,23 +83,45 @@ function App() {
         <div className="brand">
           {'<AGM/>'} Notes
         </div>
+        <div className='btn-actions'>
+          <span className="btn-add">
+            My pages
+            <span className="add-icon" onClick={addNewPage}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20"><path fill="currentColor" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z" /></svg>
+            </span>
+          </span>
+        </div>
         <ul className='ul-sidebar'>
-          <li>📱 Nequi</li>
+
           <li>
-            🗓️ Personal tasks
+            🗓️ Monthly notes
             <ul>
               <li>Junio</li>
               <li>Julio</li>
               <li>Agosto</li>
+              <li>Septiembre</li>
+              <li>Octubre</li>
             </ul>
           </li>
-          <li>🟨 Banitsmo</li>
-          <li>🏦 Modulares AVAL</li>
-          <li>💸 Facturación</li>
+          {
+
+            pages && pages.map((page) => {
+              return <li key={page.id} onClick={()=>{selectPage(page)}}>{page.name}</li>
+            })
+
+          }
+
         </ul>
+
       </div>
+
       <div className="content">
-        <AgmEditor></AgmEditor>
+        <div className='page'>
+          <h1 className="page-title" ref={editableTitle} onBlur={handleBlur} contentEditable='true' spellCheck='false' suppressContentEditableWarning={true}>
+            {(pageSelected.name=='welcome' || pageSelected.name=='blank') ? '🏡 Bienvenido' : pageSelected.name}
+          </h1>
+          <AgmEditor pageKey={pageSelected.id}></AgmEditor>
+        </div>
       </div>
 
 
--- src/Components/AgmEditor.jsx
+++ src/Components/AgmEditor.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import EditorJS from '@editorjs/editorjs';
 // import Header from '@editorjs/header';
 import List from '@editorjs/list';
@@ -17,107 +17,120 @@ import Delimiter from '@editorjs/delimiter';
 import Marker from '@editorjs/marker';
 
 
-export const AgmEditor = () => {
-
-
+export const AgmEditor = ({ pageKey }) => {
     const editorRef = useRef(null);
     const editorInstance = useRef(null);
 
     useEffect(() => {
-        const savedData = localStorage.getItem('agm');
 
-        editorInstance.current = new EditorJS({
-            holder: editorRef.current,
-            tools: {
-                // header: Header,
-                header: {
-                    class: Header,
-                    shortcut: 'CMD+SHIFT+H',
-                    config: {
-                        placeholder: 'Enter a header',
-                        levels: [1, 2, 3, 4],
-                        defaultLevel: 1,
-                        allowAnchor: true,
-                        anchorLength: 100,
-                    }
-                },
-                alert: {
-                    class: Alert,
-                    inlineToolbar: true,
-                    shortcut: 'CMD+SHIFT+A',
-                    config: {
-                        alertTypes: ['primary', 'secondary', 'info', 'success', 'warning', 'danger', 'light', 'dark'],
-                        defaultType: 'primary',
-                        messagePlaceholder: 'Enter something',
+        const initializeEditor = async () => {
+            const savedData = localStorage.getItem(pageKey);
+
+            if (editorInstance.current) {
+                try {
+                    await editorInstance.current.isReady;
+                    editorInstance.current.destroy();
+                } catch (e) {
+                    console.error('ERROR editor cleanup', e);
+                }
+            }
+
+            editorInstance.current = new EditorJS({
+                holder: editorRef.current,
+                tools: {
+                    header: {
+                        class: Header,
+                        shortcut: 'CMD+SHIFT+H',
+                        config: {
+                            placeholder: 'Enter a header',
+                            levels: [1, 2, 3, 4],
+                            defaultLevel: 1,
+                            allowAnchor: true,
+                            anchorLength: 100,
+                        },
+                    },
+                    alert: {
+                        class: Alert,
+                        inlineToolbar: true,
+                        shortcut: 'CMD+SHIFT+A',
+                        config: {
+                            alertTypes: [
+                                'primary',
+                                'secondary',
+                                'info',
+                                'success',
+                                'warning',
+                                'danger',
+                                'light',
+                                'dark',
+                            ],
+                            defaultType: 'primary',
+                            messagePlaceholder: 'Enter something',
+                        },
+                    },
+                    Marker: {
+                        class: Marker,
+                        shortcut: 'CMD+SHIFT+M',
+                    },
+                    toggle: {
+                        class: ToggleBlock,
+                        inlineToolbar: true,
+                    },
+                    delimiter: Delimiter,
+                    table: Table,
+                    list: List,
+                    image: SimpleImage,
+                    code: Code,
+                    codeBox: {
+                        shortcut: 'CMD+SHIFT+C',
+                        class: CodeBox,
+                        config: {
+                            themeName: 'atom-one-dark', // Optional
+                            useDefaultTheme: 'dark', // Optional. This also determines the background color of the language select drop-down
+                        },
+                    },
+                    nestedList: NestedList,
+                    checklist: {
+                        class: Checklist,
+                        inlineToolbar: true,
                     },
                 },
-                Marker: {
-                    class: Marker,
-                    shortcut: 'CMD+SHIFT+M',
-                  },
-                toggle: {
-                    class: ToggleBlock,
-                    inlineToolbar: true,
-                },
-                delimiter: {
-                    class:Delimiter,
-                    
-                },
-                table: Table,
-                list: List,
-                image: SimpleImage,
-                code: Code,
-                codeBox: {
-                    shortcut: 'CMD+SHIFT+C',
-                    class: CodeBox,
-                    config: {
-                        themeName: 'atom-one-dark', // Optional
-                        useDefaultTheme: 'dark' // Optional. This also determines the background color of the language select drop-down
-                    }
-                },
-                nestedList: NestedList,
-                checklist: {
-                    class: Checklist,
-                    inlineToolbar: true,
-                },
-            },
-            autofocus: true,
-            onChange: handleSave,
-            data: savedData ? JSON.parse(savedData) : {}
-        });
+                autofocus: true,
+                onChange: () => handleSave(pageKey),
+                data: savedData ? JSON.parse(savedData) : {},
+            });
+        };
+
+        initializeEditor();
 
         return () => {
-            editorInstance.current.isReady
-                .then(() => {
-                    editorInstance.current.destroy();
-                })
-                .catch((e) => console.error('ERROR editor cleanup', e));
+            if (editorInstance.current) {
+                editorInstance.current.destroy();
+                editorInstance.current = null;
+            }
         };
-    }, []);
+    }, [pageKey]);
 
+    const handleSave = (key) => {
+        if (editorInstance.current) {
 
-    const handleSave = () => {
-        editorInstance.current.save().then((outputData) => {
-            console.log('Article data: ', outputData);
-            localStorage.setItem('agm', JSON.stringify(outputData));
-        }).catch((error) => {
-            console.log('Saving failed: ', error);
-        });
+            if(key=='0'){
+                alert('Agregué un título a la página')
+                return;
+            }
+
+            editorInstance.current
+                .save()
+                .then((outputData) => {
+                    console.log('Article data: ', outputData);
+                    console.log('Saving:', key);
+                    localStorage.setItem(key, JSON.stringify(outputData));
+                })
+                .catch((error) => {
+                    console.log('Saving failed: ', error);
+                });
+        }
     };
 
-    function highlightText(text) {
-        // Regular expression to match text within square brackets
-        const regex = /\[(.*?)\]/g;
-        let newText = text.replace(regex, '<span class="highlight">$1</span>');
-        return newText;
-    }
-
-    return (
-        <div className='page'>
-            <h1 className="page-title">
-                🗒️ Título página
-            </h1>
-            <div id="editorjs" ref={editorRef} style={{ border: '0px solid gray' }}></div>
-        </div>
-    )
-}
+    return <div id="editorjs" spellCheck='false' ref={editorRef} style={{ border: '0px solid gray' }}></div>;
+};
